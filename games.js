473f814051a48a82e9537d41c5faf75608f955b3
Peter Jablonski <pwjablonski@gmail.com>
2018-12-08 16:09:03 -0500

    Update games.js

--- games.js
+++ games.js
@@ -33,7 +33,40 @@ var game1 = {
   }
 }
 
+var game2 = {
+  name: "Game 2",
+  initialState :{
+    character: {
+      direction: "right",
+      location: { yPos: 0, xPos: 0}
+    },
+    balls: [
+      {yPos: 2, xPos: 3},
+      {yPos: 4, xPos: 6}
+    ],
+    board: {columns: 8, rows: 8},
+    walls: [
+      {yPos: 1, xPos: 3, direction: "horizontal"},
+      {yPos: 3, xPos: 6, direction: "horizontal"},
+      {yPos: 4, xPos: 5, direction: "vertical"}
+    ]
+  },
+  solutionState: {
+    character: {
+      direction: "right",
+      location: {yPos: 0, xPos: 0,}
+    },
+    balls: [],
+    board: {columns: 8,rows: 8},
+    walls: [
+      {yPos: 1, xPos: 3, direction: "horizontal"},
+      {yPos: 3, xPos: 6, direction: "horizontal"},
+      {yPos: 4, xPos: 5, direction: "vertical"}
+    ]
+  }
+}
+
 var games = [
   game1,
-  game1, 
+  game2, 
 ]
