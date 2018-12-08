56d59c9876f76fd76105a769cb7d13509d1efe62
Peter Jablonski <pwjablonski@gmail.com>
2018-12-08 13:17:42 -0500

    Update games.js

--- games.js
+++ games.js
@@ -1,9 +1,6 @@
 var games = [
-  {
-   name: "Game 1",
-   initialState: 1,
-   solutionState: 2,
-  },  
+  {name: "Game 1", initialState: 1, solutionState: 2},
+  {name: "Game 1", initialState: 3, solutionState: 4}, 
 ]
 
 function loadGame(gameIndex){
